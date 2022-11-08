e5c17e091d8fd8ecca3db0e97317caee55dfffe2
Mario Sergio <msbm-@hotmail.com>
2022-11-08 15:19:18 -0300

    addclass02

---
+++ JavaScript/class -02-operations/script.js
@@ -0,0 +1,18 @@
+let num1 = 3
+let num2 = 2
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+console.log(num1 ** num2)
+
+console.log(--num1)
+console.log(++num1 + num2)
+
+
+
+
+
+
