d28130d74c43f07836963fe9b500bbce53e53001
Mario Sergio <msbm-@hotmail.com>
2022-11-10 16:07:52 -0300

    addclass3

---
+++ JavaScript/class-03-functions/challeng/script.js
@@ -0,0 +1,4 @@
+// Faça um programa que execute o IMC de cada paciente
+// Os pacientes deverão estar em uma lista e conter as propriedades:
+// name, height, weight (nome , peso e altura)
+// Escreva na tela o nome do paciente e o seu IMC
---
+++ JavaScript/class-03-functions/script.js
@@ -0,0 +1,60 @@
+// Function Statements
+
+// >>>> Named <<<<<<
+
+// function myTest() { //parameters
+//     console.log ("testando a fonção")
+// }
+
+// myTest()
+
+
+// myTest()
+// myTest()
+
+// //--------------------------------
+
+// function somar(num1, num2) {
+//     let soma = num1 + num2 // Atenção para o operador soma
+//     return soma
+// }
+
+// // console.log(soma)
+// console.log(somar(20,30))
+
+
+// // >>>> Anonymous <<<<
+
+// let sum = function (num1, num2) {
+//     let soma = num1 + num2 // Atenção para o operador soma
+//     return soma
+// }
+
+//
+
+// >>> Arrow <<<<
+
+
+// let sum = (num1, num2) => num1 + num2 
+
+// console.log(sum(2,3))
+
+
+// >>>> Constructor <<<<
+
+function Car(brandName, motorPower){
+    this.brand = brandName
+    this.motor = motorPower
+    this.walk =() => console.log(runnig)}
+
+
+    const uno = new Car("Fiat", "1.4")
+
+    console.log(uno.brand)
+    console.log(uno.motor)
+    console.log(uno.walk)
+
+
+    
+
+
