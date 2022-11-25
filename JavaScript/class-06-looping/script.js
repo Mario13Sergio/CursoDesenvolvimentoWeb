61f48cf05928ed88acc862cdc59fcc6dcbd8a3c5
mario sergio <msbm-@hotmail.com>
2022-11-25 16:11:28 -0300

    add-exercicio

--- JavaScript/class-06-looping/script.js
+++ JavaScript/class-06-looping/script.js
@@ -6,14 +6,14 @@
 // let count = 0 //flag
 
 // while (count <= 10) {
-//         if (count > 2 && count < 8) {
-//             count ++
-//             // continue
-//             break
-//         }
-//             if (count % 2 == 0) {
-//         console.log(count)
-//     }
+        // if (count > 2 && count < 8) {
+        //     count ++
+        //     // continue
+        //     break
+        // }
+        //     if (count % 2 == 0) {
+        // console.log(count)
+    // }
 //     count++ // container
 // }
 // do {
@@ -33,14 +33,32 @@
 
 // FOR
 
-for (let count = 0; count <= 10; count++) { // increment factor
-        if (count > 2 && count < 8) {
-            // count++ // Caution, two increments!
-            continue
-            // break
-        }
+// for (let count = 0; count <= 10; count++) { // increment factor
+//         if (count > 2 && count < 8) {
+//             // count++ // Caution, two increments!
+//             continue
+//             // break
+//         }
 
-        if (count % 2 == 0) {
-            console.log(count)
-        }
-}
+//         if (count % 2 == 0) {
+//             console.log(count)
+//         }
+// }
+
+
+// FOR (OF, IN)
+
+// const team = 'Brasil'
+// const team = {name: 'Brasil', cups: 5}
+// const teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
+
+
+// for(let value of team){
+//     console.log(value)
+// }
+
+// for(let index in team){
+    // console.log(team[index])
+// }
+
+// console.log(team)
---
+++ JavaScript/exercico/script.js
@@ -0,0 +1,26 @@
+
+// A: 90 - 100
+// B: 80 - 89
+// C: 70 - 79
+// D: 60 - 69
+// E:  < - 60
+
+
+function averageCalculate(note1, note2){ return (note1+note2)/2
+}
+
+function abcConvert(avarage){ return toString(avarage)
+
+}
+function estudantSituation(noteABC){ return 'aqui'
+
+}
+ let aluno = window.prompt("nome do aluno")
+
+let note1 = Number(window.prompt( "primeira nota"))
+let note2 = Number(window.prompt( "segunda nota"))
+let media = Number(window.prompt ("media"))
+document.writ (`${aluno} recebeu ${media}`)
+
+
+
