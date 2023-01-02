b105cd8b9d2f61629eaca76282e7a68d67fb1fda
mario sergio <msbm-@hotmail.com>
2023-01-02 16:41:17 -0300

    add-react

---
+++ React/class-01-fundamentals/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import './style.css'
+export function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Class React Fudamentals</h1>
+      </header>
+      <main>
+        <article>
+          <section>
+            <h2> Paragrafo 01</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sint maiores. Dolorum, enim mollitia saepe dolor culpa itaque consectetur non quam officia! Commodi aspernatur quam nihil unde libero facere nisi?
+            </p>
+            <section>
+              <h2> Paragrafo 02</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, iure? Repellat quidem recusandae vero tempore dolor. Ratione autem non voluptatem neque necessitatibus, nulla modi porro facere tenetur consequatur, temporibus consequuntur.
+              </p>
+            </section>
+          </section>
+        </article>
+      </main>
+    </div>
+  )
+}
+
+ 
