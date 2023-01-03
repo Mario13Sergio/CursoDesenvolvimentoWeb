a8c63c1da56a26ff08c4e21db3c87beea506d392
mario sergio <msbm-@hotmail.com>
2023-01-03 16:19:37 -0300

    add-react2

---
+++ React/base-xml/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import "./Home.css"
+import IconReact from "../images/react.svg"
+import React from "react"
+
+export function Home() {
+  return (
+    <div className="container">
+      <h1>Class React Base JSX</h1>  
+      <img src={IconReact} alt="" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi reprehenderit voluptates quisquam repellat minus veritatis illum nesciunt voluptas ab. Reiciendis odio numquam veniam quam ad asperiores, cum optio maxime minima!
+      </p> 
+    </div>
+  )
+}
+
+ 
