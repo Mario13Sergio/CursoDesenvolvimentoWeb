63dec6e9838c7a290a4fabcf63690af6ad6dc4c8
mario13sergio <msbm-@hotmail.com>
2023-02-15 16:19:09 -0300

    add-params

---
+++ Node.JS/class-03-params/src/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+app.listen(port, console.log(`Server is runninng on PORT ${port}`));
+
+app.use(express.json());
+const routes = require('./routes');
+app.use(routes);
